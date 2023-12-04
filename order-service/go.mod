e4dbeed799824de6f519ccdcaaa9d2825eaca78d
Yan <yanyakovtsev@gmail.com>
2023-12-04 22:13:41 +0300

    add use-case layer for order service

--- order-service/go.mod
+++ order-service/go.mod
@@ -1,3 +1,5 @@
 module github.com/HeadGardener/TaxiApp/order-service
 
 go 1.21.0
+
+require github.com/google/uuid v1.4.0
---
+++ order-service/internal/services/order.go
@@ -0,0 +1,84 @@
+package services
+
+import (
+	"context"
+	"errors"
+	"github.com/HeadGardener/TaxiApp/order-service/internal/models"
+	"github.com/google/uuid"
+)
+
+var (
+	ErrNotDriversOrder = errors.New("this order not of this driver")
+)
+
+type UserServiceGRPCClient interface {
+	AcceptOrder(ctx context.Context, userID, orderID, driverID, status string) error
+	CompleteOrder(ctx context.Context, userID, orderID, status string) error
+}
+
+type OrderStorage interface {
+	GetByID(ctx context.Context, orderID string) (models.Order, error)
+	Save(ctx context.Context, order models.Order) (string, error)
+	AddComment(ctx context.Context, orderID, comment string) error
+	UpdateStatus(ctx context.Context, orderID, status string) error
+	UpdateRating(ctx context.Context, orderID string, rating float64) error
+}
+
+type OrderService struct {
+	client       UserServiceGRPCClient
+	orderStorage OrderStorage
+}
+
+func NewOrderService(client UserServiceGRPCClient, orderStorage OrderStorage) *OrderService {
+	return &OrderService{
+		client:       client,
+		orderStorage: orderStorage,
+	}
+}
+
+func (s *OrderService) CreateOrder(ctx context.Context, order models.Order) (string, error) {
+	{
+		order.ID = uuid.NewString()
+		order.Status = models.Creating
+	}
+
+	return s.orderStorage.Save(ctx, order)
+}
+
+func (s *OrderService) AddComment(ctx context.Context, orderID, comment string) error {
+	return s.orderStorage.AddComment(ctx, orderID, comment)
+}
+
+func (s *OrderService) ProcessOrder(ctx context.Context, orderID, driverID, status string) error {
+	order, err := s.orderStorage.GetByID(ctx, orderID)
+	if err != nil {
+		return err
+	}
+
+	if order.DriverID != driverID {
+		return ErrNotDriversOrder
+	}
+
+	if err = s.client.AcceptOrder(ctx, order.UserID, orderID, driverID, status); err != nil {
+		return err
+	}
+
+	return s.orderStorage.UpdateStatus(ctx, orderID, status)
+}
+
+func (s *OrderService) CompleteOrder(ctx context.Context, driverID, orderID, status string, rating float64) error {
+	order, err := s.orderStorage.GetByID(ctx, orderID)
+	if err != nil {
+		return err
+	}
+
+	if order.DriverID != driverID {
+		return ErrNotDriversOrder
+	}
+
+	if err = s.client.CompleteOrder(ctx, order.UserID, orderID, status); err != nil {
+		return err
+	}
+
+	return s.orderStorage.UpdateRating(ctx, orderID, rating)
+}
